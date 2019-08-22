d328c427401004d9d146100a850b9f28bc94964d
Fadhlan Hazmi <fadhlan.hazmi@ui.ac.id>
2019-08-22 15:55:45 +0700

    add path pattern to regexrule

--- elastalert/modules/regex.py
+++ elastalert/modules/regex.py
@@ -2,14 +2,31 @@ from elastalert.ruletypes import RuleType
 import re
 
 class RegexRule(RuleType):
+        def match_regex(self, pattern, message):
+                return bool(re.search(pattern, message))
+
+        def get_path(self, message):
+                self.paths = {
+                        ".*products.*": "ce-products",
+                        ".*arisan.*": "ce-arisan",
+                        ".*agent\/platform.*": "mps",
+                        ".*location.*": "location",
+                        ".*logistic.*": "logistic",
+                        ".*order.*": "ce-ordering",
+                        ".*agent.*": "GK"
+                }
+
+                for pattern in self.paths:
+                        if self.match_regex(pattern, message):
+                                return self.paths[pattern]
+                
+                
         def add_data(self, data):
                 for document in data:
                         message = document['message']
-                        if bool(re.search(".*arisan.*",message)):
-                            document['target'] = "arisan" 
                         error_pattern = ".*HTTP.{5}50.*"
-                        match = bool(re.search(error_pattern,message)) 
-                        if match:
+                        if self.match_regex(error_pattern, message):
+                                document['target'] = self.get_path(message)
                                 self.add_match(document)
 
         def get_match_str(self, match):
